0b5582b6d8217670d8f489c02af3eb74ef31b67b
Alejandro Medina <medinalex13@gmail.com>
2024-01-30 08:19:43 -0500

    Setting up the Django Project

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# Quantum-Dog-Walker
+# Quantum Dog Walker
+
 A Django-based website where users can book dog walkers from various corners of the cosmos. Each with their unique, eccentric backstories.
---
+++ quantum_dog_walker/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for quantum_dog_walker project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "quantum_dog_walker.settings")
+
+application = get_wsgi_application()
